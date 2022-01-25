e2c8b506b3e936686c2ec7f0cb7f2fd9e6b87894
Tim Goloshchapov <tim.goloschapov@gmail.com>
2022-01-24 21:35:40 -0600

    episode comp done

--- src/components/tests/Episode.test.js
+++ src/components/tests/Episode.test.js
@@ -2,14 +2,37 @@ import React from 'react';
 import '@testing-library/jest-dom/extend-expect';
 import { render, screen } from '@testing-library/react';
 import Episode from './../Episode';
+import { ProgressPlugin } from 'webpack';
+
+const testEpisode = {
+  id: 1,
+  image: 'https://i.ibb.co/2FsfXqM/stranger-things.png',
+  name: '',
+  season: 1,
+  number: 1,
+  summary: 'young boy mysteriously disappears',
+  runtime: 1
+}
+
+const noPictureEpisode = {
+  id: 1,
+  image: '',
+  name: '',
+  season: 1,
+  number: 1,
+  summary: 'young boy mysteriously disappears',
+  runtime: 1
+}
 
 
 test("renders without error", () => {
-  render(<Episode episode={{}}/>);
+  render(<Episode episode={testEpisode}/>);
 });
 
+
+
 test("renders the summary text passed as prop", ()=>{
-  render(<Episode episode={{id: 1}}/>);
+  render(<Episode episode={testEpisode}  />);
 
   const summaryText = screen.queryByText(/young boy mysteriously disappears/i)
   
@@ -18,4 +41,12 @@ test("renders the summary text passed as prop", ()=>{
   expect(summaryText).toBeVisible();
 });
 
-test("renders default image when image is not defined", ()=>{});
+
+
+test("renders default image when image is not defined", ()=>{
+  render(<Episode episode={noPictureEpisode}/>);
+
+  const defaultImage = screen.queryByAltText('https://i.ibb.co/2FsfXqM/stranger-things.png')
+
+  expect(defaultImage).toBeVisible();
+});
