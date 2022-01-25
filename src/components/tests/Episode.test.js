acc5c1309486eea40626f571f0f23e49e60a20aa
Tim Goloshchapov <tim.goloschapov@gmail.com>
2022-01-24 19:59:42 -0600

    first test written

--- src/components/tests/Episode.test.js
+++ src/components/tests/Episode.test.js
@@ -4,8 +4,18 @@ import { render, screen } from '@testing-library/react';
 import Episode from './../Episode';
 
 
-test("renders without error", () => {});
+test("renders without error", () => {
+  render(<Episode episode={{}}/>);
+});
 
-test("renders the summary test passed as prop", ()=>{});
+test("renders the summary text passed as prop", ()=>{
+  render(<Episode episode={{id: 1}}/>);
+
+  const summaryText = screen.queryByText(/young boy mysteriously disappears/i)
+  
+  expect(summaryText).toBeInTheDocument();
+  expect(summaryText).toBeTruthy();
+  expect(summaryText).toBeVisible();
+});
 
 test("renders default image when image is not defined", ()=>{});
